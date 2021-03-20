949b7675c0a826a49b7036dfd8ae092ec4667edc
emouty <erwan.moutymbo@gmail.com>
2021-03-20 21:49:47 +0100

    feat: add led strip and physical button support
    - allow control of the light from a physical button connected to a GPIO
    pin
    - added support for led strip

--- octoprint_octolight/__init__.py
+++ octoprint_octolight/__init__.py
@@ -4,11 +4,28 @@ from __future__ import absolute_import, unicode_literals
 import octoprint.plugin
 from octoprint.events import Events
 import flask
-
+import board
+from rpi_ws281x import Color, PixelStrip, ws
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
+# GPIO.cleanup()
+# GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+
+# constants
+BUTTON_PIN = "button_pin"
+LIGHT_PIN = "light_pin"
+INVERTED_OUTPUT = "inverted_output"
+PREVIOUS_BUTTON_PIN = "previous_button_pin"
+IS_LED_STRIP = "is_led_strip"
+NB_LEDS = "nb_leds"
+
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_CHANNEL = 0
+LED_STRIP = ws.WS2812_STRIP
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+
 
 class OctoLightPlugin(
 		octoprint.plugin.AssetPlugin,
@@ -18,15 +35,22 @@ class OctoLightPlugin(
 		octoprint.plugin.SettingsPlugin,
 		octoprint.plugin.EventHandlerPlugin,
 		octoprint.plugin.RestartNeedingPlugin
+    octoprint.plugin.ShutdownPlugin
 	):
 
-	light_state = False
+    # variables
+    light_state = False
+    pixels = None
 
-	def get_settings_defaults(self):
-		return dict(
-			light_pin = 13,
-			inverted_output = False
-		)
+    def get_settings_defaults(self):
+        return dict(
+            light_pin=19,
+            button_pin=2,
+            previous_button_pin=2,
+            inverted_output=False,
+            is_led_strip=False,
+            nb_leds=20,
+        )
 
 	def get_template_configs(self):
 		return [
@@ -43,84 +67,125 @@ class OctoLightPlugin(
 			#less=["less/octolight.less"]
 		)
 
-	def on_after_startup(self):
-		self.light_state = False
-		self._logger.info("--------------------------------------------")
-		self._logger.info("OctoLight started, listening for GET request")
-		self._logger.info("Light pin: {}, inverted_input: {}".format(
-			self._settings.get(["light_pin"]),
-			self._settings.get(["inverted_output"])
-		))
-		self._logger.info("--------------------------------------------")
-
-		# Setting the default state of pin
-		GPIO.setup(int(self._settings.get(["light_pin"])), GPIO.OUT)
-		if bool(self._settings.get(["inverted_output"])):
-			GPIO.output(int(self._settings.get(["light_pin"])), GPIO.HIGH)
-		else:
-			GPIO.output(int(self._settings.get(["light_pin"])), GPIO.LOW)
-
-		#Because light is set to ff on startup we don't need to retrieve the current state
-		"""
-		r = self.light_state = GPIO.input(int(self._settings.get(["light_pin"])))
-        if r==1:
-                self.light_state = False
-        else:
-                self.light_state = True
-
-        self._logger.info("After Startup. Light state: {}".format(
-                self.light_state
+    def on_after_startup(self):
+        self.light_state = False
+        self._logger.info("--------------------------------------------")
+        self._logger.info("OctoLight started, listening for GET request")
+        self._logger.debug("GPIO Mode: {}".format(
+            GPIO.getmode()
         ))
-        """
+        self._logger.info("Light pin: {}, inverted_input: {}, button pin: {}".format(
+            self._settings.get([LIGHT_PIN]),
+            self._settings.get([INVERTED_OUTPUT]),
+            self._settings.get([BUTTON_PIN])
+        ))
+        self._logger.info("--------------------------------------------")
+
 
 		self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self.light_state))
 
-	def on_api_get(self, request):
-		# Sets the GPIO every time, if user changed it in the settings.
-		GPIO.setup(int(self._settings.get(["light_pin"])), GPIO.OUT)
+        # Enabling watch to the default button pin
+        self.enable_watch_button(self._settings.get([BUTTON_PIN]))
 
+    def on_api_get(self, request):
+        self._logger.info("Got request. Light state: {}".format(
+            self.light_state
+        ))
 
-		self.light_state = not self.light_state
+        if self._settings.get([BUTTON_PIN]) != self._settings.get([PREVIOUS_BUTTON_PIN]):
+            # stop watching on the previous pin
+            GPIO.remove_event_detect(
+                self._settings.get([PREVIOUS_BUTTON_PIN]))
+            # enable watching on the new pin
+            self.enable_watch_button(self._settings.get([BUTTON_PIN]))
+            self._settings.set([PREVIOUS_BUTTON_PIN],
+                               self._settings.get([BUTTON_PIN]))
 
-		# Sets the light state depending on the inverted output setting (XOR)
-		if self.light_state ^ self._settings.get(["inverted_output"]):
-			GPIO.output(int(self._settings.get(["light_pin"])), GPIO.HIGH)
-		else:
-			GPIO.output(int(self._settings.get(["light_pin"])), GPIO.LOW)
+        if bool(self._settings.get([IS_LED_STRIP])):
 
-		self._logger.info("Got request. Light state: {}".format(
-			self.light_state
-		))
+            # enabling led strip on selected pin
+            self.pixels = PixelStrip(int(self._settings.get([NB_LEDS])), int(self._settings.get(
+                [LIGHT_PIN])), LED_FREQ_HZ, LED_DMA, bool(self._settings.get([INVERTED_OUTPUT])), LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+            self.pixels.begin()
+        else:
 
-		self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self.light_state))
+            # Sets the GPIO every time, if user changed it in the settings.
+            GPIO.setup(int(self._settings.get([LIGHT_PIN])), GPIO.OUT)
+
+        self.change_light_state(None)
+
+        self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self.light_state))
 
 
 		return flask.jsonify(status="ok")
 
 	def on_event(self, event, payload):
-		if event == Events.CLIENT_OPENED:
 			self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self.light_state))
+		if event == Events.CLIENT_OPENED:
 			return
 
-	def get_update_information(self):
-		return dict(
-			octolight=dict(
-				displayName="OctoLight",
-				displayVersion=self._plugin_version,
 
-				type="github_release",
-				current=self._plugin_version,
+    def enable_watch_button(self, button):
+        self._logger.info("watching events on pin : {}".format(
+            button
+        ))
+        GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(button, GPIO.FALLING,
+                              callback=self.change_light_state, bouncetime=200)
 
-				user="gigibu5",
-				repo="OctoLight",
-				pip="https://github.com/gigibu5/OctoLight/archive/{target}.zip"
-			)
-		)
+    def change_light_state(self, channel):
+        self.light_state = not self.light_state
+
+        # Sets the light state depending on the inverted output setting (XOR)
+        if bool(self._settings.get([IS_LED_STRIP])):
+            if self.light_state:
+                self._logger.debug(
+                    "Led strip mode is enabled, will turn on the led strip")
+                self.colorWipe(self.pixels, Color(255, 255, 255))
+            else:
+                self._logger.info(
+                    "Led strip mode is enabled, will turn off the led strip")
+                self.colorWipe(self.pixels, Color(0,0,0))
+        elif self.light_state ^ self._settings.get([INVERTED_OUTPUT]):
+            GPIO.output(int(self._settings.get([LIGHT_PIN])), GPIO.HIGH)
+        else:
+            GPIO.output(int(self._settings.get([LIGHT_PIN])), GPIO.LOW)
+
+        self._logger.debug("Light state switched to : {}".format(
+            self.light_state
+        ))
+
+
+    def get_update_information(self):
+        return dict(
+            octolight=dict(
+                displayName="OctoLight",
+                displayVersion=self._plugin_version,
+
+                type="github_release",
+                current=self._plugin_version,
+
+                user="emouty",
+                repo="OctoLight",
+                pip="https://github.com/emouty/OctoLight/archive/{target}.zip"
+            )
+        )
+           
+    def on_shutdown(self):
+        # release GPIO pin on shutdown
+        GPIO.cleanup()
+
+    # Define functions which animate LEDs in various ways.
+    def colorWipe(self, strip, color):
+        """Wipe color across display a pixel at a time."""
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, color)
+            strip.show()
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoLightPlugin()
 
 __plugin_hooks__ = {
-	"octoprint.plugin.softwareupdate.check_config":
-	__plugin_implementation__.get_update_information
+    "octoprint.plugin.softwareupdate.check_config":
+        __plugin_implementation__.get_update_information
 }
