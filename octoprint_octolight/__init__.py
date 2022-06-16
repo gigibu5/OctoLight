7f8ec65c8784c4a85aeffafd792a21128d8268b7
Steven Thomson <thomst08@hotmail.com>
2022-06-16 18:19:29 +1000

    Setup Delay functions
    - Implemented Delay turning off
    - Setup option in settings to adjust time to turning off
    - Implemented API calls to enable turning on light with a delay turning off

--- octoprint_octolight/__init__.py
+++ octoprint_octolight/__init__.py
@@ -5,6 +5,9 @@ import octoprint.plugin
 from octoprint.events import Events
 import flask
 
+import math
+from octoprint.util import RepeatedTimer
+
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
@@ -21,11 +24,13 @@ class OctoLightPlugin(
 	):
 
 	light_state = False
+	delayed_state = None
 
 	def get_settings_defaults(self):
 		return dict(
 			light_pin = 13,
-			inverted_output = False
+			inverted_output = False,
+			delay_off = 5
 		)
 
 	def get_template_configs(self):
@@ -47,9 +52,10 @@ class OctoLightPlugin(
 		self.light_state = False
 		self._logger.info("--------------------------------------------")
 		self._logger.info("OctoLight started, listening for GET request")
-		self._logger.info("Light pin: {}, inverted_input: {}".format(
+		self._logger.info("Light pin: {}, inverted_input: {}, Delay Time: {}".format(
 			self._settings.get(["light_pin"]),
-			self._settings.get(["inverted_output"])
+			self._settings.get(["inverted_output"]),
+			self._settings.get(["delay_off"])
 		))
 		self._logger.info("--------------------------------------------")
 
@@ -80,6 +86,7 @@ class OctoLightPlugin(
 		GPIO.setup(int(self._settings.get(["light_pin"])), GPIO.OUT)
 
 		self.light_state = not self.light_state
+		self.stopTimer()
 
 		# Sets the light state depending on the inverted output setting (XOR)
 		if self.light_state ^ self._settings.get(["inverted_output"]):
@@ -116,12 +123,71 @@ class OctoLightPlugin(
 
 			return flask.jsonify(state=self.light_state)
 
+		elif action == "delayOff":
+			self.delayed_off_setup()
+			return flask.jsonify(state=self.light_state)
+
+		elif action == "delayOffStop":
+			self.delayed_off()
+			return flask.jsonify(state=self.light_state)
+
 		else:
 			return flask.jsonify(error="action not recognized")
+
+	#This stops the current timer, this does not control the light
+	def stopTimer(self):
+		if self.delayed_state is not None:
+			self._logger.info("Stopping schedule")
+			self.delayed_state.cancel()
+			self.delayed_state = None
+
+		return
+
+	#This sets up the timer, this does not control the light
+	#Check if the timer is already running, if so, stop it, then set it up with a new time
+	def startTimer(self, mins):
+		if math.isnan(int(mins)):
+			self._logger.info("Error: Received value that is not an int: {}".format(
+				mins
+			))
+			return
+		
+		self.stopTimer()
+
+		self._logger.info("Setting up schedule")
+		self.delayed_state = RepeatedTimer(int(mins) * 60, self.delayed_off)
+		self.delayed_state.start()
+		self._logger.info("Time till shutoff: {} seconds".format(
+			int(mins) * 60
+		))
+
+		return
+
+	#Setup the light to shutoff when called
+	def delayed_off(self):
+		if self.light_state:
+			self.light_toggle()
+
+		self.stopTimer()
+		
+		return
+
+	#Setup the light to turn on then off after a set time
+	def delayed_off_setup(self):
+		if not self.light_state:
+			self.light_toggle()
+					#self._logger.info("int(self._settings.get(["delay_off"])) * 60")
+
+		self.startTimer(self._settings.get(["delay_off"]))
+
+		return
 
 	def on_event(self, event, payload):
 		if event == Events.CLIENT_OPENED:
 			self._plugin_manager.send_plugin_message(self._identifier, dict(isLightOn=self.light_state))
+			return
+		if event == Events.PRINT_STARTED:
+			self.delayed_off_setup()
 			return
 
 	def get_update_information(self):
